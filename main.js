944aa06def34df89a76789f6478d7710f4597b8b
ninii <wuy0951@gmail.com>
2024-09-09 23:52:16 +0800

    new banner

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 import './assets/scss/all.scss';
-
+// import Swiper JS
+// import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+const swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1.15,
+      centeredSlides: true,
+      cssMode: true,
+    //   autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false
+    //   },
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+      breakpoints: {
+        992: {
+          slidesPerView: 1.15, // 較大螢幕的設置
+        },
+        0: {
+          slidesPerView: 1, // 手機版的設置，顯示一個內容
+        },
+      },
+    });
 console.log("Hello world!");
