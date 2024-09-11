ff99fde68fab1ab41f0f7ab68a37f991e951e1a8
ninii <wuy0951@gmail.com>
2024-09-11 16:19:03 +0800

    new index

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ import './assets/scss/all.scss';
 import 'swiper/css';
 const swiper = new Swiper(".mySwiper", {
       slidesPerView: 1.15,
+      spaceBetween: 40,
       centeredSlides: true,
       cssMode: true,
-    //   autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false
-    //   },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
       navigation: {
         nextEl: ".swiper-next",
         prevEl: ".swiper-prev",
@@ -29,4 +30,57 @@ const swiper = new Swiper(".mySwiper", {
         },
       },
     });
+
+ const swiper2 = new Swiper(".classSwiper", {
+    slidesPerView: 1.01,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+    pagination: {
+      el: ".swiper2-pagination",
+      dynamicBullets: true,
+    },
+});
+
+const swiper3 = new Swiper(".buySwiper", {
+    
+    slidesPerGroup:1,
+    spaceBetween: 24,
+    centeredSlides: true,
+    cssMode: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination-buy",
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+      992: {
+        slidesPerView: 5, // 較大螢幕的設置
+      },
+      0: {
+        slidesPerView: 1.2, // 手機版的設置，顯示一個內容
+      },
+    },
+  });
+
+  const swiper4 = new Swiper(".teacherSwiper", {
+    slidesPerView: 1.01,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+    pagination: {
+      el: ".swiper2-pagination",
+      dynamicBullets: true,
+    },
+});
 console.log("Hello world!");
