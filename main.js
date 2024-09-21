89a0f2fba74dc14f6181c4461b3a9e755e0fdb8a
ninii <wuy0951@gmail.com>
2024-09-21 23:57:37 +0800

    add codeclass

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ const swiper3 = new Swiper(".buySwiper", {
     keyboard: true,
     breakpoints: {
       992: {
-        slidesPerView: 5, // 較大螢幕的設置
+        slidesPerView: 4, // 較大螢幕的設置
       },
       0: {
         slidesPerView: 1.2, // 手機版的設置，顯示一個內容
@@ -81,16 +81,39 @@ const swiper3 = new Swiper(".buySwiper", {
       dynamicBullets: true,
     },
 });
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    AOS.init({
-      // 效果配置（可不寫）
-      useClassNames: true,
-      initClassName: false,
-      animatedClassName: 'animate__animated',
-      duration:100,
-    });
-  }, 2000); // 添加一段小延遲
+
+const swiper5 = new Swiper(".codeclassSwiper", {
+    
+  slidesPerGroup:2,
+  spaceBetween: 24,
+  centeredSlides: true,
+  cssMode: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  navigation: {
+    nextEl: ".swiper-next-hot",
+    prevEl: ".swiper-prev-hot",
+  },
+  pagination: {
+    el: ".swiper-pagination-codeclass",
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 5, // 較大螢幕的設置
+    },
+    0: {
+      slidesPerView: 1.8, // 手機版的設置，顯示一個內容
+    },
+  },
+});
+
+AOS.init({
+  duration:1000, 
+  // once:true,       //讓動畫只觸發一次
 });
 
 console.log("Hello world!");
